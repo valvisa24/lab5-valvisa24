0eeb439fb5fc6adbabf4a50e1cd99affa5f12d43
Valentina Vidal <Valentinavisa@unisabana.edu.co>
2026-03-17 21:54:50 -0500

    Agrego cambios de exer.py

--- multiple_files.py
+++ multiple_files.py
@@ -1,10 +1,8 @@
 from utils import *
-mensaje_original = input("Please type your message\n")
+mensaje = input("Please type your message\n")
 
-mensaje_invertido = flip(mensaje_original)
-
-cantidad_aes = count_letters(mensaje_original)
+mensaje_invertido = flip(mensaje)
+cantidad_aes = count_letters(mensaje, 'a')
 
 mensaje_codificado = str(mensaje_invertido) + str(cantidad_aes)
-
 print(f"Your encoded message is: {mensaje_codificado}")
