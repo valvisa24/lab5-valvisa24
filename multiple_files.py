1dabfac12ac3a6c1c5d4503b4a17f20618ac9d75
Valentina Vidal <Valentinavisa@unisabana.edu.co>
2026-03-17 22:56:43 -0500

    Agrego cambios de exer.py

--- multiple_files.py
+++ multiple_files.py
@@ -1,13 +1,7 @@
 from utils import *
 
-def main():
-    message = input("Please type your message\n")
-    
-    flipped_message = flip(message)
-    count_a = count_letters(message, 'a')
-    
-    encoded_message = flipped_message + str(count_a)
-    
-    print(f"Your encoded message is: {encoded_message}")
+mensaje = input("Please type your message\n")
+invertido = flip(mensaje)
+conteo = count_letters(mensaje)
 
-main()
+print(f"Your encoded message is: {invertido}{conteo}")
