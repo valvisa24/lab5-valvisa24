e7b207e7d36030829ae152eb8f8af85254ae9bbb
Valentina Vidal <Valentinavisa@unisabana.edu.co>
2026-03-17 22:32:19 -0500

    Agrego cambios de exer.py

--- mystery_module.py
+++ mystery_module.py
@@ -1,5 +1,9 @@
-# FREEZE CODE BEGIN
-def transform_data(x: int, y: float, z: str) -> float:
-    result = (x * y) / (len(z) + 1) + sum(ord(c) for c in z) % 10
-    return round(result, 2)
-# FREEZE CODE END
+from mystery_module import transform_data
+
+x = int(input("Enter an integer for x: "))
+y = float(input("Enter a float for y: "))
+
+resultado = transform_data(x, y, "quiz_test")
+
+
+print(resultado)
