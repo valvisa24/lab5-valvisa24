1e7778557a5a80e32a6940f7ff59b1625297e2a6
Valentina Vidal <Valentinavisa@unisabana.edu.co>
2026-03-17 22:45:44 -0500

    Agrego cambios de exer.py

--- caller.py
+++ caller.py
@@ -1,8 +1,1 @@
-import mystery_module
-
-x = int(input())
-y = float(input())
-
-resultado = mystery_module.transform_data(x, y, "quiz_test")
-
-print(resultado)
+from mystery_module import transform_data
