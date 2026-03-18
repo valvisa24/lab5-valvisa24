1f20d076099983fe6989c8a95beac9462d362840
Valentina Vidal <Valentinavisa@unisabana.edu.co>
2026-03-17 22:48:48 -0500

    Agrego cambios de exer.py

--- multiple_files.py
+++ multiple_files.py
@@ -1,8 +1,10 @@
 from utils import flip, count_letters
 
 message = input("Please type your message\n")
+
 flipped_message = flip(message)
 count_a = count_letters(message, 'a')
+
 encoded_message = flipped_message + str(count_a)
 
-print("Your encoded message is:", encoded_message)
+print(f"Your encoded message is: {encoded_message}")
