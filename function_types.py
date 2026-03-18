e3b2e76d8b02afe15ba6354c99cb40c292cdf0ab
Valentina Vidal <Valentinavisa@unisabana.edu.co>
2026-03-17 21:28:59 -0500

    Agrego cambios de exer.py

--- function_types.py
+++ function_types.py
@@ -2,3 +2,8 @@ def list_shift(lista_flotantes, valor_sumar):
 
     for i in range(len(lista_flotantes)):
         lista_flotantes[i] += valor_sumar
+
+
+def calc_avg(lista_flotantes):
+    conteo = len(lista_flotantes)
+    return float(promedio)
